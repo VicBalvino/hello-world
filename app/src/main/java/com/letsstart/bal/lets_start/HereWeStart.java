641bae1707da683deab7ba3971f76c8b439651a1
VicBalvino <victor.mbalvino@gmail.com>
2016-09-03 21:27:45 -0500

    uploading app folder
    Upssorry, I missed these files, now the project is complete

---
+++ app/src/main/java/com/letsstart/bal/lets_start/HereWeStart.java
@@ -0,0 +1,13 @@
+package com.letsstart.bal.lets_start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HereWeStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_here_we_start);
+    }
+}
